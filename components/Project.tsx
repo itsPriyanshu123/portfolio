3c9b427e2c53cd3828a29fcc1b47357b63c3cda4
itsPriyanshu123 <priyanshujee2017@gmail.com>
2023-07-07 01:30:31 -0700

    skill done

---
+++ components/Project.tsx
@@ -0,0 +1,34 @@
+export default function Project() {
+    const projects = [1, 2, 3]
+    return (
+        <div className="h-screen relative flex flex-col text-left md:flex-row overflow-hidden max-w-full justify-evenly mx-auto">
+
+            <h4 className="top-24 tracking-[20px] text-2xl absolute text-gray-500 uppercase">Project</h4>
+
+            <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+                {
+                    projects.map((project, i) => {
+                        return (
+                            <div className="w-screen flex flex-col justify-center items-center flex-shrink-0 snap-center p-20 md:p-44 h-screen space-y-5">
+                                <img
+                                    className="h-56 w-56"
+                                    src="https://images.unsplash.com/photo-1585700072131-f7c76261fbb6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=801&q=80"
+                                    alt="netflix image"
+                                />
+                                <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+                                    <h4 className="text-4xl font-semibold text-center">
+                                        <span className="underline decoration-[#F7AB0A]/50">Case study {i + 1} of {projects.length}</span>
+                                    </h4>
+                                    <p className="text-lg text-center md:text-left">Lorem ips Hnimior,uiorat eaque odit natus minus possimus adipisci dignissimos nulla commodi accusamus? Soluta dicta a molestias quam aperiam voluptatibus tenetur est unde. Rem labore distinctio architecto est incidunt molestiae fuga asperiores, animi at praesentium nesciunt libero obcaecati exercitationem veritatis recusandae tempore nam reprehenderit a. Delectus numquam quam possimus molestiae. Deserunt natus ad vero? Rerum distinctio tenetur necessitatibus porro sed, dolor ducimus cumque iste in doloribus modi laudantium? Quaerat magni doloremque culpa, iusto voluptate autem voluptatum velit, minus exercitationem alias placeat aliquid nam consequuntur!</p>
+                                </div>
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="top-[30%] w-full absolute bg-[#F7AB0A]/10 left-0 h-[300px] -skew-y-12">
+            </div>
+        </div>
+    )
+}
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,5 +1,31 @@
-export default function Skill(){
+//  @use-client
+'use client'
+import { motion } from 'framer-motion'
+
+type props={
+    directionLeft:boolean,
+}
+export default function Skill({directionLeft}:props) {
     return (
-        <h3>Skill</h3>
+        <div className='group relative flex cursor-pointer'>
+            <motion.img
+            initial={{
+                x:directionLeft?-200:200 ,
+                opacity:0,
+            }}
+            transition={{duration: 1}}
+            whileInView={{opacity:1,x:0}}
+
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRww1IQ_zIyo6xaGZnweymZOo2GxpKbzCow_BvWGvQL&s"
+            className='rounded-full border object-cover border-gray-300 h-24 w-24 filter xl:h-32 xl:w-32 md:w-28 md:h-28 group-hover:grayscale transition duration-300 ease-in-out'
+            />
+            <div className='absolute opacity-0 group-hover:opacity-80 h-24 w-24 xl:w-32 xl:h-32 md:28 md:h-28 rounded-full z-0  transition duration-300 ease-in-out group-hover:bg-white'>
+            <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold opacity-100 text-red-950'>100</p>
+        </div>
+            </div>
+    
+
+        </div>
     )
 }
